ff7a701d21a747c9e802c3779097f74c9e6bda8a
olonyl <olony.rocha@contollo.net>
2022-01-26 09:58:01 -0600

    Added Recaptcha Method Extension to Handle Dev and Release mode

---
+++ Sixeyed.Extensions.Advanced.Stubs/Recaptcha.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Recaptcha
+{
+    public static class RecaptchaExtensions
+    {
+        public static string GenerateCaptcha(this HtmlHelper html, string id, string theme)
+        {
+            return String.Format("<div id='{0}'><p>reCAPTCHA goes here, theme: {1}</p></div>", id, theme);
+        }
+    }
+
+    public class RecaptchaControlMvc
+    {
+        public class CaptchaValidatorAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                filterContext.ActionParameters["captchaValid"] = (object)true;
+                base.OnActionExecuting(filterContext);
+            }
+        }
+    }
+}
---
+++ Sixeyed.Extensions.Advanced.Web/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Recaptcha;
+
+using System.Web.Mvc;
+
+namespace Sixeyed.Extensions.Advanced.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost, RecaptchaControlMvc.CaptchaValidator]
+        public ActionResult Submit(bool captchaValid)
+        {
+            if (!captchaValid)
+            {
+                ModelState.AddModelError("", "reCAPTCHA invalid");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View("ThankYou");
+            }
+
+            return View("Index");
+        }
+    }
+}
